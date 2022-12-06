8af5af824c40268031f514c9e90750ff33668ccf
kaleemrao <kaleemrao417@outlook.com>
2022-12-06 22:55:13 +0500

    EDA

--- EDA.py
+++ EDA.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
-import seaborn as sns
 from pandas_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
 
